c7208d395d885bb578e4da4ddff623b50726122e
Aashimashri <60563679+Aashimashri@users.noreply.github.com>
2020-12-15 07:50:00 +0530

    Update measures java

--- app/src/main/java/com/example/corona19/measures/Measures.java
+++ app/src/main/java/com/example/corona19/measures/Measures.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-
 import com.example.corona19.R;
 import com.example.corona19.measures.Precautions;
 import com.example.corona19.measures.Symptom;
